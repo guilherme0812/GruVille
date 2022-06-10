d2e05bb9053701010d57310422365e18a8480bc0
guilherme eduardo <guilherme09112001@gmail.com>
2022-06-09 23:10:32 -0300

    Adicionando utils

--- src/screens/Map/Marker/index.jsx
+++ src/screens/Map/Marker/index.jsx
@@ -1,21 +1,15 @@
 import { View } from 'react-native';
 import { Marker as MarkerContainer, Callout } from 'react-native-maps';
+import {categories} from '../../../utils/components'
 
 import { Pin, PinImage } from './styles';
 
 const Marker = ({ title, category, pinImage, ...props }) => {
-	const categorys = [
-		{ id: '001', title: 'cinema', image: require('../../../../assets/icons/cinema-icon.png') },
-		{ id: '002', title: 'música', image: require('../../../../assets/icons/music-icon.png') },
-		{ id: '003', title: 'teatro', image: require('../../../../assets/icons/cinema-icon.png') },
-		{ id: '004', title: 'tecnologia', image: require('../../../../assets/icons/cinema-icon.png') },
-		{ id: '005', title: 'arte', image: require('../../../../assets/icons/cinema-icon.png') },
-	]
 
 	return (
 		<MarkerContainer {...props}  >
 			<Pin>
-				<PinImage source={categorys.find(element => element.title === category).image} />
+				<PinImage source={categories.find(element => element.title === category).image} />
 			</Pin>
 		</MarkerContainer>
 	)
--- src/screens/Map/index.jsx
+++ src/screens/Map/index.jsx
@@ -4,6 +4,7 @@ import Header from "./Header"
 import Marker from "./Marker"
 import { BottomSheet } from "../../components"
 import Details from "./Details"
+import {data} from '../../utils/data'
 
 import { MapContainer } from "./styles"
 
@@ -15,33 +16,6 @@ export default function Map({ navigation, ...props }) {
         latitudeDelta: 0.095,
         longitudeDelta: 0.095,
     })
-
-    const data = [
-        {
-            cover: { uri: "https://files.nsctotal.com.br/s3fs-public/graphql-upload-files/festival-danca-joinville_3.jpg?_r.2Hx3E4zg9IeReCRCe_MyBJs3osjzD" },
-            title: "Festival de Dança 2022",
-            local: "Centreventos Cau Hansen",
-            address: "Av. José Vieira, 315 - América",
-            date: new Date("2022/07/19"),
-            startTime: "20:00",
-            endTime: "22:00",
-            description: "Uma breve descrição do evento clicado anteriormente pelo usuário.",
-            cordinate: { latitude: -26.3051, longitude: -48.8461 },
-            category: "música"
-        },
-        {
-            cover: { uri: "https://cafeviagem.com/wp-content/uploads/2019/05/vinicolas-em-Santa-Catarina-d3.jpg" },
-            title: "Degustação de vinhos",
-            local: "Vinícola D'alture",
-            address: "Rodovia SC 114",
-            date: new Date("2022/07/20"),
-            startTime: "07:30",
-            endTime: "09:00",
-            description: "Uma breve descrição do evento clicado anteriormente pelo usuário.",
-            cordinate: { latitude: -26.2900, longitude: -48.8200 },
-            category: "cinema"
-        }
-    ]
 
     return (
         <View>
